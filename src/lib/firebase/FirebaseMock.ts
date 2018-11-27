ba2eb46f1c95100f6ddf4362d2a198debaee5aa6
Dave Weaver <dave@markkup.com>
2018-11-27 15:46:57 -0500

    Re-enable firebase-server

--- src/lib/firebase/FirebaseMock.ts
+++ src/lib/firebase/FirebaseMock.ts
@@ -8,12 +8,15 @@ class FirebaseMock implements IFirebase {
     protected config: IFirebaseConfig = {} as IFirebaseConfig;
 
     public async init(config: IFirebaseConfig): Promise<void> {
+        if (process.env.NODE_ENV !== 'test') {
+            throw new Error('Mock not supported outside of testing');
+        }
         try {
             if (firebase.apps && firebase.apps.length > 0) {
                 return;
             }
-            // const FirebaseServer = require('firebase-server');
-            // new FirebaseServer(5000, 'localhost', null);
+            const FirebaseServer = require('firebase-server');
+            new FirebaseServer(5000, 'localhost', null);
             this.config = config;
             await firebase.initializeApp({
                 databaseURL: `ws://localhost:5000`,
